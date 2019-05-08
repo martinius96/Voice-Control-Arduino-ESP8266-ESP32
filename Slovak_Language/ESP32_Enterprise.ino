6192bbc064ed6e9bad139a1490251384e06fae86
Martin <martinius96@gmail.com>
2019-05-08 23:55:22 +0200

    Update ESP32_Enterprise.ino

--- Slovak_Language/ESP32_Enterprise.ino
+++ Slovak_Language/ESP32_Enterprise.ino
@@ -1,3 +1,10 @@
+/*|---------------------------------|*/
+/*|Projekt: Ovládanie hlasom        |*/
+/*|Autor: Martin Chlebovec          |*/
+/*|E-mail: martinius96@gmail.com    |*/
+/*|Web: https://arduino.php5.sk     |*/
+/*|Licencia pouzitia: MIT           |*/
+/*|---------------------------------|*/
 #include "esp_wpa2.h"
 #include <WiFi.h>
 String line;
